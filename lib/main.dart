e6fac7db34424063e3a48632bef7d522d06cd881
Pratyush Puri <79128035+PratyushPuri@users.noreply.github.com>
2023-05-03 16:02:38 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'World',
+      title: 'World Clock',
       theme: ThemeData(
         primarySwatch: Colors.green,
         textTheme: const TextTheme(
